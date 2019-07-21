c48a4367cadf428b1d3268459d0f71bcc7e08a26
Lauri Lubi <lauri.lubi@tretton37.com>
2019-07-21 10:54:39 +0200

    tutorial defaults

---
+++ Tutorial Defaults/PlatformerTemplate/Scripts/Mechanics/PlayerController.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Platformer.Gameplay;
+using static Platformer.Core.Simulation;
+using Platformer.Model;
+using Platformer.Core;
+
+namespace Platformer.Mechanics
+{
+    /// <summary>
+    /// This is the main class used to implement control of the player.
+    /// It is a superset of the AnimationController class, but is inlined to allow for any kind of customisation.
+    /// </summary>
+    public class PlayerController : KinematicObject
+    {
+        public AudioClip jumpAudio;
+        public AudioClip respawnAudio;
+        public AudioClip ouchAudio;
+        public string playerId = "none";
+        public float defaultMaxSpeed = 3;
+        public float defaultJumpTakeOffSpeed = 6.2f;
+        public float defaultHrzAcc = 4;
+
+
+        internal JumpState jumpState = JumpState.Grounded;
+        internal bool stopJump;
+        internal new Collider2D collider;
+        internal new AudioSource audio;
+        internal Health health;
+        internal float stunnedUntil;
+        internal int jumpStepCount;
+        internal bool isCatcher;
+        internal float catchableAfter;
+        internal float jumpableAfter;
+        internal float hrzAcc;
+        internal float maxSpeed;
+        internal float jumpTakeOffSpeed;
+        internal float hrzFlippedUntil;
+        internal float vrtFlippedUntil;
+        internal float teleportableAfter;
+        internal bool isDropping;
+        internal float droppableAfter;
+
+
+        bool jump;
+        Vector2 move;
+        SpriteRenderer spriteRenderer;
+        internal Animator animator;
+        PlatformerModel model;
+
+        public const float minimum = 0.01f;
+        public const float dropStunPeriod = 2.5f;
+
+        public Bounds Bounds => collider.bounds;
+
+        void Awake()
+        {
+            health = GetComponent<Health>();
+            audio = GetComponent<AudioSource>();
+            collider = GetComponent<Collider2D>();
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            animator = GetComponent<Animator>();
+            model = GetModel<PlatformerModel>();
+
+            hrzAcc = defaultHrzAcc;
+            maxSpeed = defaultMaxSpeed;
+            jumpTakeOffSpeed = defaultJumpTakeOffSpeed;
+
+            if (playerId == "P1") MakeCatcher(null);
+        }
+
+        protected override void Update()
+        {
+            var xAxis = GetXAxis();
+            if (Mathf.Abs(xAxis) < minimum)
+            {
+                // is stopping
+                if (move.x > minimum)
+                {
+                    // still going right
+                    move.x = Math.Max(0, move.x - hrzAcc * 3 * Time.deltaTime);
+                }
+                else if (move.x < -minimum)
+                {
+                    // still going left
+                    move.x = Math.Min(0, move.x + hrzAcc * 3 * Time.deltaTime);
+                }
+                else
+                {
+                    move.x = 0;
+                }
+            }
+            else
+            {
+                // is moving
+                move.x += xAxis * hrzAcc * Time.deltaTime;
+                move.x = Cap(move.x);
+            }
+
+            var yAxis = GetYAxis();
+            if (yAxis > 0.2f && CanJump())
+            {
+                jumpState = JumpState.PrepareToJump;
+            }
+            else if (yAxis < -0.2f && CanDrop())
+            {
+                isDropping = true;
+                droppableAfter = Time.time + 2 * dropStunPeriod;
+                velocity.x *= 2;
+                velocity.y = -1.5f * defaultJumpTakeOffSpeed * model.jumpModifier;
+            }
+            //else if (Input.GetButtonUp("Jump"))
+            //{
+            //    stopJump = true;
+            //    Schedule<PlayerStopJump>().player = this;
+            //}
+
+            UpdateJumpState();
+
+            if (isDropping)
+            {
+                var hasCollisionWithLevelItems = hitBufferCount > 0 && hitBuffer.Take(hitBufferCount).Any(_ => _.collider.gameObject.GetComponent<PlayerController>() == null);
+                if (hasCollisionWithLevelItems || velocity.y >= 0)
+                {
+                    isDropping = false;
+                    stunnedUntil = Time.time + 0.75f * dropStunPeriod;
+                }
+            }
+
+            //if (droppableAfter < Time.time) isDropping = false;
+
+            base.Update();
+        }
+
+        private float GetXAxis()
+        {
+            if (Time.time < stunnedUntil)
+            {
+                return 0;
+            }
+
+            var xAxis = Input.GetAxis($"{playerId}-Horizontal");
+            if (xAxis < -minimum) xAxis = -1;
+            else if (xAxis > minimum) xAxis = 1;
+            else xAxis = 0;
+
+            if (Time.time < hrzFlippedUntil) xAxis *= -1;
+            return xAxis;
+        }
+
+        private float GetYAxis()
+        {
+            if (Time.time < stunnedUntil)
+            {
+                return 0;
+            }
+
+            var yAxis = Input.GetAxis($"{playerId}-Vertical");
+
+            if (Time.time < vrtFlippedUntil)
+            {
+                yAxis *= -1;
+            }
+            return yAxis;
+        }
+
+        private float Cap(float value, float minValue = -1, float maxValue = 1)
+        {
+            if (value < minValue) return minValue;
+            if (value > maxValue) return maxValue;
+            return value;
+        }
+
+        bool CanJump()
+        {
+            if (Time.time < jumpableAfter) return false;
+            if (isDropping) return false;
+
+            if (isCatcher && jumpStepCount < 3) return true;
+            if (IsGrounded) return true;
+
+            return false;
+        }
+
+        bool CanDrop()
+        {
+            return Time.time >= droppableAfter;
+        }
+
+        void UpdateJumpState()
+        {
+            jump = false;
+            switch (jumpState)
+            {
+                case JumpState.PrepareToJump:
+                    jumpState = JumpState.Jumping;
+                    jump = true;
+                    stopJump = false;
+                    if (IsGrounded) jumpStepCount = 0;
+                    jumpStepCount++;
+                    jumpableAfter = Time.time + 0.25f;
+                    break;
+                case JumpState.Jumping:
+                    if (!IsGrounded)
+                    {
+                        Schedule<PlayerJumped>().player = this;
+                        jumpState = JumpState.InFlight;
+                    }
+                    break;
+                case JumpState.InFlight:
+                    if (IsGrounded)
+                    {
+                        Schedule<PlayerLanded>().player = this;
+                        jumpState = JumpState.Landed;
+                    }
+                    break;
+                case JumpState.Landed:
+                    jumpState = JumpState.Grounded;
+                    jumpStepCount = 0;
+                    break;
+            }
+        }
+
+        protected override void ComputeVelocity()
+        {
+            if (jump)
+            {
+                velocity.y = defaultJumpTakeOffSpeed * model.jumpModifier;
+                jump = false;
+            }
+            else if (stopJump)
+            {
+                stopJump = false;
+                if (velocity.y > 0)
+                {
+                    velocity.y = velocity.y * model.jumpDeceleration;
+                }
+            }
+
+            if (move.x > minimum)
+                spriteRenderer.flipX = false;
+            else if (move.x < -minimum)
+                spriteRenderer.flipX = true;
+
+            animator.SetBool("grounded", IsGrounded);
+            animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+
+            targetVelocity = move * maxSpeed;
+        }
+
+        public void MakeCatcher(PlayerController previousCatcher)
+        {
+            isCatcher = true;
+            spriteRenderer.transform.localScale = new Vector2(0.55f, 0.55f);
+            maxSpeed = defaultMaxSpeed + 1;
+
+            if (previousCatcher == null) return;
+
+            previousCatcher.isCatcher = false;
+            previousCatcher.catchableAfter = Time.time + 0.1f;
+
+            previousCatcher.transform.localScale = new Vector2(0.4f, 0.4f);
+            previousCatcher.maxSpeed = previousCatcher.defaultMaxSpeed;
+
+            previousCatcher.TeleportRandom();
+        }
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (isDropping == false && isCatcher == false) return;
+
+            var otherPlayer = collision.gameObject.GetComponent<PlayerController>();
+            if (otherPlayer == null) return;
+
+            if (isDropping)
+            {
+                isDropping = false;
+                if (otherPlayer.stunnedUntil < Time.time)
+                {
+                    otherPlayer.stunnedUntil = Time.time + dropStunPeriod;
+                }
+            }
+
+            if (isCatcher && stunnedUntil < Time.time)
+            {
+                if (otherPlayer.catchableAfter < Time.time && otherPlayer.isDropping == false)
+                {
+                    otherPlayer.MakeCatcher(this);
+                }
+            }
+        }
+
+        public static PlayerController[] GetPlayers()
+        {
+            return new[]
+            {
+                GameObject.Find("P1").GetComponent<PlayerController>(),
+                GameObject.Find("P2").GetComponent<PlayerController>(),
+                GameObject.Find("P3").GetComponent<PlayerController>()
+            };
+        }
+
+        public static PlayerController[] GetNonChasers()
+        {
+            return GetPlayers().Where(_ => _.isCatcher == false).ToArray();
+        }
+
+        public enum JumpState
+        {
+            Grounded,
+            PrepareToJump,
+            Jumping,
+            InFlight,
+            Landed
+        }
+    }
+}
---
+++ Tutorial Defaults/PlatformerTemplate/Scripts/Mechanics/TokenInstance.cs
@@ -0,0 +1,102 @@
+using Platformer.Gameplay;
+using UnityEngine;
+using static Platformer.Core.Simulation;
+
+namespace Platformer.Mechanics
+{
+    /// <summary>
+    /// This class contains the data required for implementing token collection mechanics.
+    /// It does not perform animation of the token, this is handled in a batch by the 
+    /// TokenController in the scene.
+    /// </summary>
+    [RequireComponent(typeof(Collider2D))]
+    public class TokenInstance : MonoBehaviour
+    {
+        public AudioClip tokenCollectAudio;
+        [Tooltip("If true, animation will start at a random position in the sequence.")]
+        public bool randomAnimationStartTime = false;
+        [Tooltip("List of frames that make up the animation.")]
+        public Sprite[] idleAnimation, collectedAnimation;
+        public float respawnInterval;
+
+        internal Sprite[] sprites = new Sprite[0];
+
+        new internal SpriteRenderer renderer;
+
+        //unique index which is assigned by the TokenController in a scene.
+        internal int tokenIndex = -1;
+        internal TokenController controller;
+        //active frame in animation, updated by the controller.
+        internal int frame;
+        internal bool collected;
+        internal float respawnAt;
+
+        public const int flipTime = 5;
+
+        void Awake()
+        {
+            renderer = GetComponent<SpriteRenderer>();
+            if (randomAnimationStartTime)
+                frame = Random.Range(0, sprites.Length);
+            sprites = idleAnimation;
+        }
+
+        void Update()
+        {
+            if (respawnAt <= Time.time)
+            {
+                var gameArea = GameObject.Find("GameArea");
+                var gameAreaCollider = gameArea.GetComponent<Collider2D>();
+                var bounds = gameAreaCollider.bounds;
+
+                var x = Random.Range(bounds.min.x, bounds.max.x);
+                var y = Random.Range(bounds.min.y, bounds.max.y);
+                var position = new Vector3(x, y, 0);
+
+                renderer.transform.position = position;
+
+                collected = false;
+                sprites = idleAnimation;
+                respawnAt = Time.time + respawnInterval;
+            }
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            //only exectue OnPlayerEnter if the player collides with this token.
+            var player = other.gameObject.GetComponent<PlayerController>();
+            if (player != null) OnPlayerEnter(player);
+        }
+
+        void OnPlayerEnter(PlayerController player)
+        {
+            if (collected) return;
+
+            //frame = 0;
+            sprites = collectedAnimation;
+            collected = true;
+            renderer.transform.position = new Vector3(-1000, -1000, 0);
+
+            if (player.isCatcher)
+            {
+                var mousePlayers = PlayerController.GetNonChasers();
+                foreach (var mousePlayer in mousePlayers)
+                {
+                    mousePlayer.hrzFlippedUntil = Time.time + flipTime;
+                    mousePlayer.vrtFlippedUntil = Time.time + flipTime;
+                }
+            }
+
+            //send an event into the gameplay system to perform some behaviour.
+            //var ev = Schedule<PlayerTokenCollision>();
+            //ev.token = this;
+            //ev.player = player;
+        }
+
+        //void int GetRandomAction(bool isCatcher)
+        //{
+        //    var chances = new
+
+        //}
+    }
+}
